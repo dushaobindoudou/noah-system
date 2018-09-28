5ccf776fb4ccd1fe9b1a8c63b29994852c5910a3
Jess <maochengjie1988@gmail.com>
2018-09-28 16:01:24 +0800

    leek init

---
+++ client/.leek.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "projectType": "react",
+    "prefix": "",
+    "publicPath": "/static/",
+    "dist": "../../dist",
+    "clientAlias": "client",
+    "isInlineCss": "false",
+    "leekConfig": "./.leekConfig/",
+    "leekWebpackConfigDir": "{{leekConfig}}/webpack/",
+    "leekManifsetDir": "{{leekConfig}}/manifest/",
+    "configIn": "client"
+}
